778106d1bd867b1348b98e950dcd286723ca0192
이승규 <62980952+tmzkt21@users.noreply.github.com>
2020-08-21 16:34:54 +0900

    차트데이터 띄우기 실패
    차트데이터 띄우기 실패

--- src/chart/ChartBar.js
+++ src/chart/ChartBar.js
@@ -6,41 +6,45 @@ const ChartBar = props => {
     const [totalKey,settotalKey] = useState([]);
     const [totalValue,settotalValue] =useState([]);
     const [chart,setChart] =useState({})
-    useEffect(()=>{
+    const [name,setName] = useState('')
+
+
+    const chartHandle = e => {
         axios
-            .get(`http://localhost:8080/chart/total/user`)
+            .get(`http://localhost:8080/user/data/${name}`)
             .then((res)=>{
-                alert("성공")
+                alert("성공" + res.data)
                 const keykey =[];
                 const valuesvalues =[];
                 Object.entries(res.data).forEach(([key,value])=>{
                     keykey.push(key)
                     valuesvalues.push(value)
                 })
-                 settotalKey(keykey);
-                 settotalValue(valuesvalues)
+                settotalKey(keykey);
+                settotalValue(valuesvalues)
+                alert("차트 데이터값"+totalValue)
             })
 
             .catch((err)=>{
-                alert("성공")
+                alert("실패")
                 throw err;
             })
-    },[])
+    }
     const chartData = {
         labels: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
         datasets: [
             {
-                label: '판매량',
+                label: totalKey,
                 data: totalValue,
                 backgroundColor: 'rgba(120,29,29,0.3)',
                 borderWidth: 4,
                 LineTension: 0
             },
-            {
-                label: '방문자',
-                data: totalValue,
-                backgroundColor: '#FFE4C4'
-            },
+            // {
+            //     label: '방문자',
+            //     data: totalValue,
+            //     backgroundColor: '#FFE4C4'
+            // },
         ]
     }
     const {chartValue} = props
@@ -54,6 +58,8 @@ const ChartBar = props => {
     return (
         <div>
             <h2>{chartValue}</h2>
+            <input type="text" onChange={e => setName(e.target.value)}/>
+            <input type="button" onClick={chartHandle} value={"회원조회"}/>
             <Bar
                 data={chartData}
             />
