c6a60a5376f1c8f1ed52086aa677691a0ca6e2ca
이승규 <62980952+tmzkt21@users.noreply.github.com>
2020-08-23 21:12:59 +0900

    int로 못바꾸었다.. split 시도중
    int로 못바꾸었다.. split 시도중

--- src/chart/ChartBar.js
+++ src/chart/ChartBar.js
@@ -1,54 +1,48 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {Bar} from 'react-chartjs-2'
 import axios from 'axios'
+import {string} from "prop-types";
 
 const ChartBar = props => {
     const [totalKey,setTotalKey] = useState([]);
-    let [totalValue,setTotalValue] =useState([]);
+    const [totalValue,setTotalValue] =useState([]);
     const [name,setName] = useState('')
+
     const chartHandle = e => {
-        // alert("배열 보기: "+[1,2,,3])
         axios
             .get(`http://localhost:8080/user/data/${name}`)
             .then((res)=>{
                 alert("성공" + res.data)
-                const keykey =[];
-                const valuesvalues =[];
+                const keyContainer =[];
+                const valueContainer  = [];
                 Object.entries(res.data).forEach(([key,value])=>{
-                    keykey.push(key)
-                    valuesvalues.push(value)
+                    console.log("밸류값"+value)
+                    keyContainer.push(key)
+                    valueContainer.push(value)
+                    setTotalKey(keyContainer);
+                    setTotalValue(valueContainer)
+                    totalValue.split(',').map(Number)
+                    console.log(totalValue)
                 })
-                alert("배열 값: "+valuesvalues)
-                setTotalKey(keykey);
-                setTotalValue(valuesvalues)
-                console.log(totalValue)
-                alert("차트 데이터값"+totalValue)
             })
             .catch((err)=>{
                 alert("실패")
                 throw err;
             })
     }
-    console.log(totalValue)
     const chartData = {
         labels: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
         datasets: [
             {
                 label: totalKey,
-                // data: totalValue.map(data=>(data)),
-                // data: totalValue.forEach(data=>(data)),
-                // data: totalValue={totalValue},
-                // data: "totalValue",
-                // data: ["23,42,56,15,65,15,48,98"],
-                // data: totalValue.push(totalValue),
-                // data: parseInt(totalValue),
-                data: parseInt(totalValue),
+                data: totalValue,
                 backgroundColor: 'rgba(120,29,29,0.3)',
                 borderWidth: 4,
                 LineTension: 0
             }
         ]
     }
+
     const {chartValue} = props
     const [dataType, setDataType] = useState(chartData)
     // const switchCase = (param) =>{
